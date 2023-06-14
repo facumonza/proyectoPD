e6f49b04a99f09b8a53a190a2610a56256616824
Facundo Monza <132512996+facumonza@users.noreply.github.com>
2023-06-13 22:50:19 -0300

    Add files via upload

---
+++ js/proyecto.js
@@ -0,0 +1,7 @@
+
+function cartel() {alert ("Potrero digital");}
+
+function modonocturno() {document.body.style.backgroundColor = "#5D5C5B";
+document.body.style.color = "white";}
+
+function copyright() {alert ("Página creada por Facundo Monza. E-mail: facundom.monza@gmail.com");}
